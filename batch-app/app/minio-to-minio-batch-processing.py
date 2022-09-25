3898e1fa0d52a63a01c6917a6e2c1e842bdd82ab
root <root@serv.sanctuary.local>
2022-09-25 22:04:22 +0200

    [FIX] Quick fix to gitignore file to add s3-storage data

---
+++ batch-app/app/minio-to-minio-batch-processing.py
@@ -0,0 +1,64 @@
+import os, subprocess, re
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+from datetime import datetime, timedelta
+
+
+def create_spark_session():
+    """
+    Return a sparkSession object with specific configuration
+    such as bucket path, etc.
+    """
+    SPARK_MASTER_URL = 'spark://spark:7077' # Via "/etc/hosts"
+    ETH0_IP = subprocess.check_output(["hostname", "-i"]).decode(encoding='utf-8').strip()
+    ETH0_IP = re.sub(fr'\s*127.0.0.1\s*', '', ETH0_IP) # Remove alias to 127.0.0.1, if present.
+    SPARK_DRIVER_HOST = ETH0_IP
+    os.environ["SPARK_LOCAL_IP"] = ETH0_IP
+          # NOTE: Spark driver/client-applications connecting to Spark clusters implemented
+          # as Docker containers need to set 'spark.driver.host' to be that client's IP-Address,
+          # not that client's hostname. For example, if launching a PySpark application from your
+          # desktop, use the desktop's IP-Address. REASON: Because the Docker containers won't have
+          # an "/etc/hosts" entry for said client (unless you configure it to via docker-compose(5)
+          # or similar), and so cannot resolve it's (the client's) hostname to communicate back.
+          # The "SPARK_LOCAL_IP" environment variable is set to that IP-Address as well, so
+          # that Spark, PySpark, spark-submit(1) don't complain on startup.
+    
+    
+    # Define the spark Session
+    spark = SparkSession \
+            .builder \
+            .appName("Read minio to process batch data") \
+            .config("spark.master", SPARK_MASTER_URL) \
+            .config("spark.driver/bindAddress", "0.0.0.0") \
+            .config("spark.driver.host", SPARK_DRIVER_HOST) \
+            .config("spark.sql.streaming.checkpointLocation", "s3a://spark-checkpoint/") \
+            .config("spark.submit.deployMode", "client") \
+            .config("spark.hadoop.fs.s3a.path.style.access", "true")\
+            .config("fs.s3a.connection.ssl.enabled", "false") \
+            .config("spark.hadoop.fs.s3a.access.key", "minio-root") \
+            .config("spark.hadoop.fs.s3a.secret.key", "MyStr0n8Passw04rd*") \
+            .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
+            .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+            .config("spark.sql.shuffle.partitions", "27")
+            .getOrCreate()
+    sc = spark.sparkContext
+
+    return spark, sc
+
+
+def main ():
+    """
+    Main function to execute the spark pipeline and
+    aggregations
+    """
+    spark, sc = create_spark_session()
+    day = datetime.today().strftime("%Y-%m-%d")
+
+    # Read first bucket written by the stream app, for a specific day
+    df = spark.read.load("s3a://flights-enriched/").where(col("day").isin(day))
+    df.show()
+
+if __name__ == "__main__":
+    main()
---
+++ collector/collector.py
@@ -0,0 +1,175 @@
+from FlightRadar24.api import FlightRadar24API, Flight
+from datetime import datetime
+from time import sleep
+import json
+
+
+
+
+FR_API = FlightRadar24API()
+
+
+def write_file(data: dict[dict]) -> None:
+    """ 
+    Write a dict of dict as a Json file to the path /input
+
+    parameter:
+    ---------
+
+    data: dict[dict]
+      The dictionnary containing all informations for each flight, the flight
+      id beeing the primary key of the dict
+
+    return:
+    --------
+    None
+    """
+    with open(f"/input/flight_data_{datetime.strftime(datetime.now(), '%Y-%m-%d_%H%M%S')}.json", "w") as f:
+        for line in data:
+            f.write(str(line)+"\n")
+
+def get_airline_name(airline_iata: str, airline_icao: str, airlines: dict) -> str:
+    """
+    Return the icao or the iata flight airline code, depending of the 
+    airline code returned by the api
+
+    parameters:
+    -----------
+
+    airline_iata: str
+       The 2 charaters code identifying the airline of the aircraft
+
+    airline_icao: str
+       The 3 characters code identifying the airline of the aircraft
+
+    return:
+    --------
+
+    airline_icao|airline_iata: str
+      Depending of the airlines, icao or iata code must be used
+      to identify the airline. return the corresponding code to
+      correctly identify the airline
+    """
+    if airline_icao in airlines.keys():
+        return airlines[airline_icao]
+    elif airline_iata in airlines.keys():
+        return airlines[airline_iata]
+    else:
+        return "Null"
+
+def get_airport_data(airport_iata: str, airports: dict[dict]) -> dict:
+    """
+    Return None if the airport is not found in the 
+    FlightRadar24 snapshot
+
+    parameters:
+    -------------
+    airport_iata; str
+      the airtport iata code which consist of 3 characters
+
+    aiports: dict[dict]
+      dict of airports and their coordinate, name and altitude
+
+    return:
+    -------
+    airports[key]: dict|str
+      Airports data encapsulated as a dict, such as longitude, latitude,
+      altitude, name...
+      
+    """
+    if airport_iata in airports.keys():
+        return airports[airport_iata]
+    return "Null"
+
+def extract_airports() -> dict[dict]:
+    """
+    Extract a snapshot of all the airports identified by Flightradar24
+
+    parameters:
+    ---------
+    None
+
+    return:
+    -------
+
+    airports: dict[dict]
+      A json-like formated data containing airports name, latitude, longitude, iata code,
+      country and altitude. Airport iata code is used as primary key
+    """
+    airports = {x["iata"]:x for x in FR_API.get_airports()}
+    airports["N/A"] = "Null"
+    return airports
+
+
+def extract_airlines() -> dict:
+    """
+    Extract a snapshot of all the airlines identified by Flightradar24
+
+    parameters:
+    ---------
+    None
+
+    return:
+    -------
+
+    airlines: dict
+      A json-like formated data containing airlines name and icao code
+      the icao code is used as the primary key
+    """
+    airlines = {x["ICAO"]:x["Name"] for x in FR_API.get_airlines()}
+    airlines["N/A"] = "Null"
+    return airlines
+
+
+def get_flight_enriched_data():
+    """
+    Extract flights data from FlightRadar24 API and 
+    enrich data with airline and airport data
+
+    parameters:
+    -----------
+    None
+
+    return:
+    -----------
+    
+    data: dict[dict]
+       Flights data enriched with airport name, latitude, longitude...
+       and airline name
+    """
+
+    # List[Flight]
+    # Contains all flight informations
+    flights = FR_API.get_flights()
+
+    # Get airlines and airports json-like data
+    airlines = extract_airlines()
+    airports = extract_airports()
+
+    return [{"aircraft_id":x.id,
+             "aircraft_code":x.aircraft_code, 
+             "airline_iata":x.airline_iata,
+             "airline_name": get_airline_name(x.airline_iata, x.airline_icao, airlines),
+             "callsign":x.callsign,
+             "destination_airport_iata":get_airport_data(x.destination_airport_iata, airports),
+             "origin_airport_iata":get_airport_data(x.origin_airport_iata, airports),
+             "latitude": x.latitude,
+             "longitude":x.longitude,
+             "on_ground":x.on_ground,
+             "time":x.time} for x in flights] 
+
+def main():
+    """
+    Execute the main get_flight_enriched_data as well as write
+    data to /input/flight_data.json format
+    """
+    data = get_flight_enriched_data()
+    write_file(data)
+
+
+    
+if __name__ == "__main__":
+    while True:
+        main()
+        sleep(5)
+
